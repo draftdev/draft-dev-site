6a5a852e2847e1297aa55cbbc358012ba67cf2b4
robin <rfussell17@gmail.com>
2025-02-28 16:17:37 -0800

    change gtag

--- src/components/global/analytics-provider.tsx
+++ src/components/global/analytics-provider.tsx
@@ -1,11 +1,7 @@
 'use client'
 
+import { initUserInteractionTracking, trackScroll } from '@/app/utils/analytics'
 import { useEffect, useRef } from 'react'
-import {
-  initUserInteractionTracking,
-  trackScroll,
-} from '../../app/utils/analytics'
-import GoogleAnalytics from './google-analytics'
 import GoogleTagManagerNoScript from './gtm-noscript'
 
 export default function AnalyticsProvider({
@@ -47,8 +43,62 @@ export default function AnalyticsProvider({
   return (
     <>
       <GoogleTagManagerNoScript />
-      <GoogleAnalytics />
       {children}
     </>
   )
 }
+
+// 'use client'
+
+// import { useEffect, useRef } from 'react'
+// import {
+//   initUserInteractionTracking,
+//   trackScroll,
+// } from '../../app/utils/analytics'
+// import GoogleAnalytics from './google-analytics'
+// import GoogleTagManagerNoScript from './gtm-noscript'
+
+// export default function AnalyticsProvider({
+//   children,
+// }: {
+//   children: React.ReactNode
+// }) {
+//   // Use refs to track if event listeners are initialized
+//   const scrollTrackingInitialized = useRef(false)
+//   const interactionTrackingInitialized = useRef(false)
+
+//   useEffect(() => {
+//     // Only initialize once
+//     if (!scrollTrackingInitialized.current) {
+//       // Initialize scroll tracking with IntersectionObserver
+//       const scrollCleanup = trackScroll()
+//       scrollTrackingInitialized.current = true
+
+//       return () => {
+//         scrollCleanup()
+//         scrollTrackingInitialized.current = false
+//       }
+//     }
+//   }, [])
+
+//   useEffect(() => {
+//     // Only initialize once
+//     if (!interactionTrackingInitialized.current) {
+//       const interactionCleanup = initUserInteractionTracking()
+//       interactionTrackingInitialized.current = true
+
+//       return () => {
+//         interactionCleanup()
+//         interactionTrackingInitialized.current = false
+//       }
+//     }
+//   }, [])
+
+//   return (
+//     <>
+//       <GoogleTagManagerNoScript />
+//       <GoogleAnalytics />
+//       {children}
+//     </>
+//   )
+// }
