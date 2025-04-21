5778a9d60ea32f73c6530ecc5e7549730beb80ca
robin <rfussell17@gmail.com>
2025-04-21 15:20:18 -0700

    update newsletter form

--- src/app/newsletter/page.tsx
+++ src/app/newsletter/page.tsx
@@ -1,6 +1,7 @@
-import { FormHeader } from '@/components/global/headers/form-header'
+import { LogosDark } from '@/components/media/logos-dark'
 import Testimonial from '@/components/media/testimonials/testimonial'
 import TestimonialsGroup from '@/components/media/testimonials/testimonials-group'
+import ServiceInfo from '@/components/page-components/newsletter/service-info'
 import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
@@ -19,13 +20,8 @@ export const metadata: Metadata = {
 export default function Newsletter() {
   return (
     <>
-      <div className="m-auto">
-        <FormHeader
-          title={'Sign up for our Newsletter'}
-          descriptionOne="Resources, tips, and case studies to help you reach developers."
-          descriptionTwo="Delivered to your inbox every month."
-        />
-      </div>
+      <ServiceInfo />
+      <LogosDark />
       <TestimonialsGroup />
       <Testimonial
         quote="Having draft.dev source quality technical content for the Loft Labs blog has been a competitive advantage. It's given us a steadier flow of content, which has helped our brand's visibility, and some of the posts are among the most popular ones we've published."
