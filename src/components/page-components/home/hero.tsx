b88369f1cea6fc15ffef460b7f57ef98c310c3a7
robin <rfussell17@gmail.com>
2025-06-19 14:57:18 -0700

    trying lcp fix - core web vitals failing

--- src/components/page-components/home/hero.tsx
+++ src/components/page-components/home/hero.tsx
@@ -12,50 +12,54 @@ const Hero: React.FC = () => {
           <div className="mx-auto max-w-7xl px-6 py-6 sm:py-16 lg:px-8">
             <div className="mx-auto w-full lg:flex lg:items-center lg:justify-between">
               <div className="relative max-w-2xl lg:shrink-0">
-                <div className="mt-24 py-3 sm:mt-32 lg:mt-16">
-                  <span className="mr-3 rounded-full bg-white px-3 py-1 text-sm font-semibold text-primary ring-1 ring-inset ring-primary-40">
-                    <Link href="/resources" className="">
-                      New eBook
-                    </Link>
-                  </span>
-                  <Link
-                    href="/content-marketing-engine"
-                    className="inline-flex"
-                  >
-                    <span className="inline-flex items-center text-sm text-white">
-                      How to Set Up a Content Marketing Engine in the Age of AI
-                      <ChevronRightIcon
-                        aria-hidden="true"
-                        className="size-5 text-gray-400"
-                      />
+                <div className="content-visibility-auto contain-intrinsic-size-[360px] min-h-[360px] min-w-[320px] pb-6">
+                  <div className="mt-24 py-3 sm:mt-32 lg:mt-16">
+                    <span className="mr-3 rounded-full bg-white px-3 py-1 text-sm font-semibold text-primary ring-1 ring-inset ring-primary-40">
+                      <Link href="/resources" className="">
+                        New eBook
+                      </Link>
                     </span>
-                  </Link>
-                </div>
-                <h1
-                  className="sm:header-light mb-6 pb-4 pl-3 text-left font-code text-3xl font-semibold text-white sm:pl-0"
-                  style={{ contentVisibility: 'auto' }}
-                >
-                  {' '}
-                  We are a Content Creation Agency for Technical Companies
-                </h1>
-                <p className="sm:paragraph-light rounded-lg bg-white/5 p-5 text-lg text-gray-100 sm:bg-transparent sm:p-0">
-                  Draft.dev helps Marketing and Developer Relations teams in
-                  tech businesses build trust, drive awareness, and capture leads.
-                </p>
-                <div className="mb-6 mt-16 flex flex-col items-start gap-y-4 pl-3 sm:mb-0 sm:flex-row sm:items-center sm:gap-x-6 sm:gap-y-0 lg:pl-0">
-                  <Link
-                    href="/call"
-                    className="mb-8 inline-block rounded-sm px-3.5 py-2.5 font-semibold text-white shadow-sm ring-2 ring-white hover:bg-white hover:text-gray-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary sm:mb-0 sm:text-base"
+                    <Link
+                      href="/content-marketing-engine"
+                      className="inline-flex"
+                    >
+                      <span className="inline-flex items-center text-sm text-white">
+                        How to Set Up a Content Marketing Engine in the Age of
+                        AI
+                        <ChevronRightIcon
+                          aria-hidden="true"
+                          className="size-5 text-gray-400"
+                        />
+                      </span>
+                    </Link>
+                  </div>
+                  <h1
+                    className="sm:header-light mb-6 pb-4 pl-3 text-left font-code text-3xl font-semibold text-white sm:pl-0"
+                    style={{ contentVisibility: 'auto' }}
                   >
-                    Book a Discovery Call
-                  </Link>
-                  <Link
-                    href="#lead-generation-package"
-                    className="inline-block font-semibold text-white hover:text-gray-200 sm:text-base"
-                  >
-                    See Our Lead Generation Package{' '}
-                    <span aria-hidden="true">→</span>
-                  </Link>
+                    {' '}
+                    We are a Content Creation Agency for Technical Companies
+                  </h1>
+                  <p className="sm:paragraph-light rounded-lg bg-white/5 p-5 text-lg text-gray-100 sm:bg-transparent sm:p-0">
+                    Draft.dev helps Marketing and Developer Relations teams in
+                    tech businesses build trust, drive awareness, and capture
+                    leads.
+                  </p>
+                  <div className="mb-6 mt-16 flex flex-col items-start gap-y-4 pl-3 sm:mb-0 sm:flex-row sm:items-center sm:gap-x-6 sm:gap-y-0 lg:pl-0">
+                    <Link
+                      href="/call"
+                      className="mb-8 inline-block rounded-sm px-3.5 py-2.5 font-semibold text-white shadow-sm ring-2 ring-white hover:bg-white hover:text-gray-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary sm:mb-0 sm:text-base"
+                    >
+                      Book a Discovery Call
+                    </Link>
+                    <Link
+                      href="#lead-generation-package"
+                      className="inline-block font-semibold text-white hover:text-gray-200 sm:text-base"
+                    >
+                      See Our Lead Generation Package{' '}
+                      <span aria-hidden="true">→</span>
+                    </Link>
+                  </div>
                 </div>
               </div>
               <div className="mt-14 hidden justify-end gap-8 sm:-mt-44 sm:pl-20 lg:mt-0 lg:flex lg:pl-0">
@@ -85,7 +89,6 @@ const Hero: React.FC = () => {
                           width={400}
                           src="/site/small-portrait/sales_draft_dev.jpg"
                           className="aspect-[2/3] w-full rounded-xl object-cover"
-                          priority={true}
                           loading="eager"
                         />
                       </div>
@@ -103,7 +106,6 @@ const Hero: React.FC = () => {
                           width={400}
                           src="/site/small-portrait/laptop_draft_dev.jpg"
                           className="aspect-[2/3] w-full rounded-xl object-cover"
-                          priority={true}
                           loading="eager"
                         />
                       </div>
@@ -119,7 +121,6 @@ const Hero: React.FC = () => {
                           width={400}
                           src="/site/small-portrait/commit_draft_dev.jpg"
                           className="aspect-[2/3] w-full rounded-xl object-cover"
-                          priority={true}
                           loading="eager"
                         />
                       </div>
