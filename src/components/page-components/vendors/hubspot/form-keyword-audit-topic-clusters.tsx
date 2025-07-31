fd315c79eabe69106728f584f0da4531e33e1973
Manuel Weiss <mail.mweiss@gmail.com>
2025-07-31 18:26:58 -0400

    add resource LP and form component for keyword audit and topic clusters

---
+++ src/components/page-components/vendors/hubspot/form-keyword-audit-topic-clusters.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import LoadHubSpotForm from './load-hubspot-form'
+
+export default function FormKeywordAuditTopicClusters() {
+  return (
+    <LoadHubSpotForm
+      portalId="146032007"
+      formId="12781998-8856-4a8b-a05b-e9f6b0368f56"
+      region="eu1"
+    />
+  )
+}
