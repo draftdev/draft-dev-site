2ad5d713d10b23910b53e6ab6c409c535a97c43e
Manuel Weiss <mail.mweiss@gmail.com>
2025-09-18 12:01:19 +0200

    startwork on eaxample articles landing page

---
+++ src/components/global/example-articles.tsx
@@ -0,0 +1,181 @@
+export default function ExampleArticlesList() {
+
+  return (
+    <div className="bg-gradient-brand sm:py-16">
+      <div className="mx-auto max-w-4xl px-6 py-12 sm:py-0 sm:pb-6 lg:max-w-4xl lg:text-center">
+        <h2 className="subheader-light text-5xl">Technical Content by Draft.dev</h2>
+
+        <h3 className="paragraph-light py-2 text-lg">
+          Browse through content examples we have produced for clients like Docker, JetBrains, Sinch, and others.
+        </h3>
+        <p className="paragraph-light text-sm py-6">
+          <a className="text-gray-700 bg-white focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mx-4" href="#">Tutorials</a>
+          <a className="text-gray-700 bg-white focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mx-4" href="#">Guides</a>
+          <a className="text-gray-700 bg-white focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mx-4" href="#">Persuasion</a>
+          <a className="text-gray-700 bg-white focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mx-4" href="#">Comparison</a>
+        </p>
+      </div>
+
+      <div className="my-6">
+        {' '}
+        
+        <div className="mx-auto max-w-7xl px-6 lg:px-8">
+          <a href="#">
+            <div className="rounded-3xl shadow-[inset_0_0_2px_1px_#ffffff4d]">
+              <div className="p-2">
+                <div className="overflow-hidden rounded-[20px] bg-white/5 shadow-2xl">
+                  <div className="flex flex-col lg:flex-row">
+                    <div className="flex min-h-12 w-full flex-col justify-center text-left lg:w-96">
+                      <div className="subheader-light pl-10 text-lg sm:text-4xl">
+                        <span className="block">Docker</span>
+                        <p className="font-light">Persuasion</p>
+                      </div>
+                    </div>
+                    <div className="w-full bg-white p-8 sm:p-10 lg:w-[calc(100%-384px)]">
+                      <p className="subheader-gradient text-2xl sm:text-2xl">A test link</p>
+                      <p className="text-base sm:text-lg">
+                        We analyze your industry, competitors, positioning,
+                        product features, target audience, and make sure to
+                        understand your brand and tone of voice, ensuring
+                        authentic output.
+                      </p>
+                      <div className="my-8 bg-gray-300" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </a>
+        </div>
+      </div>
+
+      <div className="my-6">
+        {' '}
+        <div className="mx-auto max-w-7xl px-6 lg:px-8">
+          <div className="rounded-3xl shadow-[inset_0_0_2px_1px_#ffffff4d]">
+            <div className="p-2">
+              <div className="overflow-hidden rounded-[20px] bg-white/5 shadow-2xl">
+                <div className="flex flex-col lg:flex-row">
+                  <div className="flex min-h-12 w-full flex-col justify-center text-left lg:w-96">
+                    <h3 className="subheader-light pl-10 text-lg sm:text-4xl">
+                      <span className="block">Step 2:</span> Content Generation
+                    </h3>
+                  </div>
+                  <div className="w-full bg-white p-8 sm:p-10 lg:w-[calc(100%-384px)]">
+                    <h4 className="subheader-gradient text-2xl sm:text-3xl">
+                      Blending AI Efficiency and Human Accuracy
+                    </h4>
+                    <p className="text-base sm:text-lg">
+                      We build expert-crafted AI workflows customized to your
+                      unique situation. We provide content types like product
+                      tutorials, videos, comparisons, how-tos, guides, and much
+                      more.
+                    </p>
+                    <div className="my-8 bg-gray-300" />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="my-6">
+        {' '}
+        <div className="mx-auto max-w-7xl px-6 lg:px-8">
+          <div className="rounded-3xl shadow-[inset_0_0_2px_1px_#ffffff4d]">
+            <div className="p-2">
+              <div className="overflow-hidden rounded-[20px] bg-white/5 shadow-2xl">
+                <div className="flex flex-col lg:flex-row">
+                  <div className="flex min-h-12 w-full flex-col justify-center text-left lg:w-96">
+                    <h3 className="subheader-light pl-10 text-lg sm:text-4xl">
+                      <span className="block">Step 3:</span> Technical Experts
+                      Review
+                    </h3>
+                  </div>
+                  <div className="w-full bg-white p-8 sm:p-10 lg:w-[calc(100%-384px)]">
+                    <h4 className="subheader-gradient text-2xl sm:text-3xl">
+                      Technical & Journalistic Expertise
+                    </h4>
+                    <p className="text-base sm:text-lg">
+                      We have over 300+ practicing software engineers reviewing
+                      your content. Our journalists and editors conduct copy
+                      edits and grammar edits to ensure your content is of the
+                      highest quality, technically correct, and authentic to
+                      your brand's voice.
+                    </p>
+                    <div className="my-8 bg-gray-300" />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="my-6">
+        {' '}
+        <div className="mx-auto max-w-7xl px-6 lg:px-8">
+          <div className="rounded-3xl shadow-[inset_0_0_2px_1px_#ffffff4d]">
+            <div className="p-2">
+              <div className="overflow-hidden rounded-[20px] bg-white/5 shadow-2xl">
+                <div className="flex flex-col lg:flex-row">
+                  <div className="flex min-h-12 w-full flex-col justify-center text-left lg:w-96">
+                    <h3 className="subheader-light pl-10 text-lg sm:text-4xl">
+                      <span className="block">Step 4:</span> Content Refreshes
+                    </h3>
+                  </div>
+                  <div className="w-full bg-white p-8 sm:p-10 lg:w-[calc(100%-384px)]">
+                    <h4 className="subheader-gradient text-2xl sm:text-3xl">
+                      Stay Relevant & Rank Well
+                    </h4>
+                    <p className="text-base sm:text-lg">
+                      It's crucial to keep your content fresh. Nobody wants to
+                      have a prospective customer use ChatGPT to get a result
+                      that shows outdated information about your product. We run
+                      recurring content analysis and technical, as well as
+                      SEO/GEO refreshes.
+                    </p>
+                    <div className="my-8 bg-gray-300" />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="my-6">
+        {' '}
+        <div className="mx-auto max-w-7xl px-6 lg:px-8">
+          <div className="rounded-3xl shadow-[inset_0_0_2px_1px_#ffffff4d]">
+            <div className="p-2">
+              <div className="overflow-hidden rounded-[20px] bg-white/5 shadow-2xl">
+                <div className="flex flex-col lg:flex-row">
+                  <div className="flex min-h-12 w-full flex-col justify-center text-left lg:w-96">
+                    <h3 className="subheader-light pl-10 text-lg sm:text-4xl">
+                      <span className="block">Step 5:</span> Analyze & Optimize
+                    </h3>
+                  </div>
+                  <div className="w-full bg-white p-8 sm:p-10 lg:w-[calc(100%-384px)]">
+                    <h4 className="subheader-gradient text-2xl sm:text-3xl">
+                      Insights and Transparency
+                    </h4>
+                    <p className="text-base sm:text-lg">
+                      We provide recurring reports and insights around your
+                      performance and apply content improvements based on our
+                      internal SEO and AI Optimization platform when new
+                      opportunities for updates present themselves
+                    </p>
+                    <div className="my-8 bg-gray-300" />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
