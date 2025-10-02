f80f986f2ee1eb4439d063f8aecc8c21402b33f8
robin <rfussell17@gmail.com>
2025-10-02 10:14:56 -0700

    add distributing content on social media form

---
+++ src/components/page-components/vendors/hubspot/form-distributing-content-on-social-media.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import LoadHubSpotForm from './load-hubspot-form'
+
+export default function FormContentMarketingEngine() {
+  return (
+    <LoadHubSpotForm
+      portalId="146032007"
+      formId="effabeb7-1670-4c40-8ec2-7fbb127bc4a1"
+      region="eu1"
+    />
+  )
+}
