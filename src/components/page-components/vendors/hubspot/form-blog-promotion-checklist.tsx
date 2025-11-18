175c79648ecd65e35ab9f7f331ae470a0fd779dd
robin <rfussell17@gmail.com>
2025-11-18 15:29:40 -0800

    add big blog promotion checklist to grid

---
+++ src/components/page-components/vendors/hubspot/form-blog-promotion-checklist.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import LoadHubSpotForm from './load-hubspot-form'
+
+export default function FormBlogPromotionChecklist() {
+  return (
+    <LoadHubSpotForm
+      portalId="146032007"
+      formId="b3f2f890-8828-4453-96d2-aa07e2d8f71d"
+      region="eu1"
+    />
+  )
+}
