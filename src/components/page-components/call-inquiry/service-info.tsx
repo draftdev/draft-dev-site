5cb10b5db2b3baa47043e6cc1b930d0fc30af0e0
robin <rfussell17@gmail.com>
2026-03-05 12:11:20 -0800

    testing new form

---
+++ src/components/page-components/call-inquiry/service-info.tsx
@@ -0,0 +1,22 @@
+import FormInquiry from '../vendors/hubspot/form-inquiry'
+
+const ServiceInfo = () => {
+  return (
+    <div className="overflow-hidden bg-white py-14 sm:py-32">
+      <div className="mx-auto max-w-7xl md:px-6 lg:px-8">
+        <div className="grid grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:grid-cols-2 lg:items-center">
+          <div className="lg:px-0 lg:pt-4 lg:pr-4">
+            <div className="mx-auto max-w-4xl sm:max-w-4xl lg:mx-0">
+              <h1 className="sm:subheader-gradient subheader-mobile-gradient px-8">
+                Reach out to us
+              </h1>
+              <FormInquiry />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ServiceInfo
---
+++ src/components/page-components/vendors/hubspot/form-inquiry.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import LoadHubSpotForm from './load-hubspot-form'
+
+export default function FormInquiry() {
+  return (
+    <LoadHubSpotForm
+      portalId="146032007"
+      formId="6e875552-7d98-41ca-ad22-e7b49c552ea4"
+      region="eu1"
+    />
+  )
+}
